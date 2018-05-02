b8c4795a0bab269c2f88b8db08dbb47cc550f694
localadmin <briz.andrew@gmail.com>
2018-05-02 12:01:36 -0400

    Fixed merge issues.

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 import csv
-from empath import Empath
-# import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from json import loads, dumps
 from operator import itemgetter
-<<<<<<< HEAD
 import pickle
-=======
->>>>>>> b3080ea94b87905e2421ab04014826d325fca5c4
+from categories import sent_categories
+from insensitive_dict_reader import InsensitiveDictReader
 
 
 WEIGHTS = {
@@ -45,59 +42,38 @@ def interpret(filename):
     regr = linear_model.BayesianRidge()
     regr.fit(train_features, train_score)
 
-    model = {
-        'regr': regr,
-        'feature_names' : feature_names
-    }
-
-    #pickle regr
-    filename = 'finalized_model.sav'
-    pickle.dump(model, open(filename, 'wb'))
-
-    loaded_model = pickle.load(open(filename, 'rb'))
-    result = loaded_model['regr'].predict(test_features)
-
-    #pickle feature_names
-
-
-    # print(result)
-
-    # testing machine learning stuff
-    # print(test_score[53])
-    # print(test_score[41])
-    # print(test_score[250])
-    # print(test_score[500])
-    predictions = regr.predict(test_features)
-    # print('-----')
-<<<<<<< HEAD
-    # print(result[53])
-=======
->>>>>>> b3080ea94b87905e2421ab04014826d325fca5c4
-    # print(predictions[41])
-    # print(predictions[250])
-    # print(predictions[500])
+    # model = {
+    #     'regr': regr,
+    #     'feature_names': feature_names
+    # }
+    #
+    # #pickle regr
+    # filename = 'finalized_model.sav'
+    # pickle.dump(model, open(filename, 'wb'))
+    #
+    # loaded_model = pickle.load(open(filename, 'rb'))
+    # result = loaded_model['regr'].predict(test_features)
 
     # The mean squared error
-    print("Mean squared error: %.2f"
-          % mean_squared_error(test_score, predictions))
-    # Explained variance score: 1 is perfect prediction
-    print('Variance score: %.2f' % r2_score(test_score, predictions))
+    # print("Mean squared error: %.2f"
+    #       % mean_squared_error(test_score, predictions))
+    # # Explained variance score: 1 is perfect prediction
+    # print('Variance score: %.2f' % r2_score(test_score, predictions))
+
     # The coefficients
     coefs = []
-    print('Coefficients: \n')
     for idx, val in enumerate(regr.coef_):
         output = {}
         output['name'] = feature_names[idx]
 
         if val > 0:
-            output['pos/neg'] = '+'
+            output['pos/neg'] = ''
         else:
             output['pos/neg'] = '-'
 
         output['value'] = val
         coefs.append(output)
     sorted_coefs = sorted(coefs, key=itemgetter('value'))
-<<<<<<< HEAD
     # #print(sorted_coefs)
     top5 = sorted(sorted_coefs[-5:], key=itemgetter('value'), reverse=True)
     # # print(top5)
@@ -113,8 +89,8 @@ def interpret(filename):
         print(item["name"])
     print("------")
     print("For more information on what this means, go to: https://repositories.lib.utexas.edu/bitstream/handle/2152/31333/LIWC2015_LanguageManual.pdf")
-=======
->>>>>>> b3080ea94b87905e2421ab04014826d325fca5c4
+
+    return regr, feature_names
 
 
 def analyze(tweets):
@@ -125,10 +101,16 @@ def analyze(tweets):
 
     # Calculate score based on retweets and favorites
     for tweet in tweets:
+        tweet_data = {}
+
         score = float(tweet['favorite_count']) * WEIGHTS['favorites']
         score = score + float(tweet['retweet_count']) * WEIGHTS['retweets']
-        tweet['score'] = score
-        scored_tweets.append(loads(dumps(tweet)))
+        tweet_data['score'] = score
+        # Filter out unnecessary data
+        for cat in sent_categories:
+            tweet_data[cat] = tweet[cat]
+
+        scored_tweets.append(loads(dumps(tweet_data)))
 
     # Sort by score and remove the top and bottom 300 to remove outliers
     scored_tweets = sorted(scored_tweets, key=itemgetter('score'))
@@ -136,17 +118,10 @@ def analyze(tweets):
 
     # Creature x(features) and y(scores) lists for ML
     for tweet in scored_tweets:
-        # Remove unnesecary properties
-        tweet.pop('id')
-        tweet.pop('created_at')
-        tweet.pop('text')
-        tweet.pop('favorite_count')
-        tweet.pop('retweet_count')
-
-        # Add to score list
+        # Add scores to score list
         scores.append(tweet.pop('score'))
 
-        # Create list of sentiment categoreis
+        # Create list of sentiment category names
         if len(feature_names) == 0:
             feature_names = list(tweet.keys())
 
@@ -161,23 +136,31 @@ def analyze(tweets):
 # def report(scores,features):
 #     print(sorted_coefs)
 
-def save(data, filename):
-    keys = data[0].keys()
-    with open(filename, 'w') as f:
-        dict_writer = csv.DictWriter(f, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(data)
+
+def get_best(filename, modelname):
+    model, features_list = interpret('data/train/analyzed/' + modelname)
+
+    with open('data/test/analyzed/' + filename, 'r') as f:
+        reader = InsensitiveDictReader(f)
+        tweets = [row for row in reader]
         f.close()
 
+    all_texts = []
+    all_features = []
+    for tweet in tweets:
+        all_texts.append(tweet['text'])
 
-<<<<<<< HEAD
-def sortDictList(arr, sort_key, reverse=False):
-    output = [(dict_[sort_key], dict_) for dict_ in arr]
-    output.sort(reverse=reverse)
-    return [dict_ for (key, dict_) in output]
-=======
+        features = []
+        for feature_name in features_list:
+            features.append(float(tweet[feature_name]))
+        all_features.append(features)
+
+    predictions = list(model.predict(all_features))
+    best_prediction = predictions.index(max(predictions))
+
     return tweets[best_prediction]['text']
 
 
 if __name__ == '__main__':
-    interpret('nytimes_liwc_filtered.csv')
+    best_tweet = get_best('test.csv', 'nytimes.csv')
+    print('----------\n' + best_tweet)
